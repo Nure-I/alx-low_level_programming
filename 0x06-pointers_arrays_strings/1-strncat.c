7ccf7692fa275f57017b14999f433749c1d3110c
Nure-I <ibrahimsaladin1@gmail.com>
2022-03-23 22:27:37 +0300

    some changes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,22 +1,32 @@
 #include "main.h"
 /**
- * *_strncpy - find the length of a string
- * @dest: pointer to the string
- * @src: pointer
- * @n: int
- * Return: destination
-*/
+ *_strncat - concatenates two strings
+ *@dest: A pointer to a character that will be changed
+ *@src: A pointer to a character that will be changed
+ *@n: value
+ *Return: dest
+ */
 
+char *_strncat(char *dest, char *src, int n)
 
-char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+int i, j;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[i] = src[i];
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
 
-for ( ; i < n; i++)
-	dest[i] = '\0';
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[i] = src[j];
+j++;
+i++;
+}
+
+dest[i] = '\0';
 
 return (dest);
 }
